7a4d59dc457040cb86d85f570ef455bc8a4d7f19
Subhankar Das <being.subh90@gmail.com>
2024-11-30 23:12:49 +0530

    Add files via upload

---
+++ Codes/climate.c
@@ -0,0 +1,20 @@
+/*Program Description
+Mr. Snowman considers the climate HOT if the temperature is above 20, otherwise he considers it as COLD. You are given the temperature C, find whether the climate is HOT or COLD.
+
+Input Format
+A single line input containing one integer. 
+
+
+Output Format
+Print the output according to the description.*/
+
+ #include <stdio.h>
+ #include <math.h>
+ 
+ int main()
+ {
+    int x;
+    scanf("%d",&x);
+    (x>20)?(printf("HOT")):(printf("COLD"));
+    return 0;
+ }
