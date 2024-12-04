b9c1dc1a9e9b2c58fd965c07176a6b3af0806686
Subhankar Das <being.subh90@gmail.com>
2024-12-04 09:31:24 +0530

    Add files via upload

--- Codes/donation.c
+++ Codes/donation.c
@@ -1,20 +1,28 @@
 /*Program Description
-In a certain month, Pavan earned X rupees while Ganapathi earned Y rupees such that Y>X.
-Since they want to end with exactly the same amount ,they decide to donate the difference between their income to a trust.Find the amout they donate in the month.
+On the occasion of World Blood Donor Day, Ram has organized an event to reward regular blood donars in Aditya
+
+If the donor has made less than or equal to 3 donations, they receive a BRONZE donor badge.
+If the donor has made more than 3 but less than equal to 6 donations, they receive a SILVER donor badge.
+If the donor has made more than 6 donations, they receive a GOLD donor badge.
+Given that a person has made X donations, find the badge they receive.
 
 Input Format
-A single line input contains two space separated integers X,Y. 
+A single line of input contains an integer X-denoting the number of blood donations the person has made.
 
 
 Output Format
-Print the output, the amount they donate in the month. */
+Print the output as: BRONZE,
+if the person has made less than or equal to 3 donations SILVER,
+if the person has made more than 3 but less than equal to 6 donations GOLD,
+if the person has made more than 6 donations.*/
 
-#include <stdio.h>
-
-int main()
-{
-    int X,Y;
-    scanf("%d%d",&X,&Y);
-    printf("%d",Y-X);
+ #include <stdio.h>
+ #include <math.h>
+ 
+ int main()
+ {
+    int x;
+    scanf("%d",&x);
+    (x>6)?(printf("GOLD")):((x>3&&x<=6)?(printf("SILVER")):(printf("BRONZE")));
     return 0;
-}
+ }
