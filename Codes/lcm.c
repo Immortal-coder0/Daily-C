ffaa84e6c97dccec93356d79f18aeecc4952e4cf
Immortalcoder0 <being.subh90@gmail.com>
2024-12-17 13:09:37 +0530

    Update existing file

--- Codes/lcm.c
+++ Codes/lcm.c
@@ -19,7 +19,22 @@ int lcm(int a, int b)
 int main()
 {
     int num1, num2;
-    printf("Enter two numbers ");
+    printf("Enter two numbers#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a,b;
+    float c,d;
+    scanf("%d %d",&a,&b);
+    scanf("%f %f",&c,&d);
+    printf("%d %d\n",a+b,a-b);
+    printf("%.1f %.1f",c+d,c-d);
+    return 0;
+}
+ ");
     scanf("%d %d", &num1, &num2);
 
     printf("LCM of %d and %d is %d\n", num1, num2, lcm(num1, num2));
