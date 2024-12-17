9539f487b15a73b3015c3869accd18332f4fbeab
Immortalcoder0 <being.subh90@gmail.com>
2024-12-17 13:36:55 +0530

    Upadate existing file

--- Codes/climate.c
+++ Codes/climate.c
@@ -1,11 +1,14 @@
 /*Program Description
+
 Mr. Snowman considers the climate HOT if the temperature is above 20, otherwise he considers it as COLD. You are given the temperature C, find whether the climate is HOT or COLD.
 
 Input Format
+
 A single line input containing one integer. 
 
 
 Output Format
+
 Print the output according to the description.*/
 
  #include <stdio.h>
